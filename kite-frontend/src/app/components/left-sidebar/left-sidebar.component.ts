67412f310acfc11f4f52a21b7d1d1831763891b5
Daorsa Hyseni <daorsahyseni@gmail.com>
2025-04-28 14:33:33 +0200

    .

--- kite-frontend/src/app/components/left-sidebar/left-sidebar.component.ts
+++ kite-frontend/src/app/components/left-sidebar/left-sidebar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-left-sidebar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.css'
 })
 export class LeftSidebarComponent {
+  isNewMode = true;
 
+  onToggleChange(): void {
+    console.log('Current mode:', this.isNewMode ? 'New' : 'Unread');
+  }
 }
