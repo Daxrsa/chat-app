caa2216572ec754351eba302025de56e51ca6269
Daorsa Hyseni <daorsahyseni@gmail.com>
2025-04-28 16:45:46 +0200

    .

--- kite-frontend/src/app/components/chat-area/chat-area.component.ts
+++ kite-frontend/src/app/components/chat-area/chat-area.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chat-area',
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './chat-area.component.css'
 })
 export class ChatAreaComponent {
+  @ViewChild('messageInput') messageInputElement!: ElementRef;
+  @ViewChild('send_msg') textareaRef!: ElementRef<HTMLTextAreaElement>;
 
+  ngAfterViewInit() {
+    if (this.messageInputElement) {
+      this.adjustTextareaHeight();
+    }
+  }
+
+  adjustTextareaHeight(event: Event | null = null) {
+    const textarea = event
+      ? event.target as HTMLTextAreaElement
+      : this.messageInputElement?.nativeElement;
+
+    if (!textarea) return;
+
+    textarea.style.height = 'auto';
+    textarea.style.height = (textarea.scrollHeight) + 'px';
+  }
+
+  clearTextarea(): void {
+    if (this.textareaRef) {
+      this.textareaRef.nativeElement.value = '';
+      this.textareaRef.nativeElement.style.height = "auto";
+    }
+  }
 }
