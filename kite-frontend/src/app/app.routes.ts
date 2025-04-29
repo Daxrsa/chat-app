6509e1fce38703635198cdbad2939909abbfdcfc
Daorsa Hyseni <daorsahyseni@gmail.com>
2025-04-29 11:36:08 +0200

    added routing

--- kite-frontend/src/app/app.routes.ts
+++ kite-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { ChatMainComponent } from './pages/chat-main/chat-main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'auth', component: AuthComponent },
+    { path: 'inbox', component: ChatMainComponent },
+    { path: '', redirectTo: '/auth', pathMatch: 'full' }, 
+    { path: '**', redirectTo: '/auth' } 
+];
