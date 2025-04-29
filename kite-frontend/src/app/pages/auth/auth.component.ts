44461d2f0476fc67e259daab41e5749e7e823508
Daorsa Hyseni <daorsahyseni@gmail.com>
2025-04-29 12:20:04 +0200

    save

--- kite-frontend/src/app/pages/auth/auth.component.ts
+++ kite-frontend/src/app/pages/auth/auth.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
-
+  loading = false;
+  errorMessage = '';
+  showPassword = false;
 }
